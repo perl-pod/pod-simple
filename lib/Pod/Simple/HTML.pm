39376eec35a9408ce5b95fb86ec105889c4ab7fd
Gabor Szabo <szabgab@gmail.com>
2013-03-08 18:09:53 +0200

    added attribute top_anchor to hold the string that is added to each file

--- lib/Pod/Simple/HTML.pm
+++ lib/Pod/Simple/HTML.pm
@@ -75,6 +75,7 @@ __PACKAGE__->_accessorize(
  'html_header_before_title',
  'html_header_after_title',
  'html_footer',
+ 'top_anchor',
 
  'index', # whether to add an index at the top of each page
     # (actually it's a table-of-contents, but we'll call it an index,
@@ -209,6 +210,7 @@ sub new {
     "<!-- start doc -->\n",
   );
   $new->html_footer( qq[\n<!-- end doc -->\n\n</body></html>\n] );
+  $new->top_anchor( "<a name='___top' class='dummyTopAnchor' ></a>\n" );
 
   $new->{'Tagmap'} = {%Tagmap};
 
@@ -312,7 +314,7 @@ sub do_beginning {
 sub _add_top_anchor {
   my($self, $text_r) = @_;
   unless($$text_r and $$text_r =~ m/name=['"]___top['"]/) { # a hack
-    $$text_r .= "<a name='___top' class='dummyTopAnchor' ></a>\n";
+    $$text_r .= $self->top_anchor || '';
   }
   return;
 }
@@ -989,6 +991,13 @@ and including the opening <title> tag. The following call will set it to be a si
 file:
 
   $p->html_header_before_title('<html><head><title>');
+
+=head2 top_anchor
+
+By default Pod::Simple::HTML adds a dummy anchor at the top of the HTML.
+You can change it by calling
+
+  $p->top_anchor('<a name="zz" >');
 
 =head2 html_h_level
 
