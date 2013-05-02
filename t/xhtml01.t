perl-pod/pod-simple

commit fc97ecd5bbb7c02a796e6852078292d020c2d0af

Add `encoding()` and `deteted_encoding()`.
This allows formatters to output in the original encoding, should they wish.
The value of the detected or configured encoding is now also kept in the
output tree. Thanks to Patrice Dumas for the patch!

lib/Pod/Simple.pm
@@ -113,6 +113,35 @@ sub any_errata_seen {  # good for using as an exit() value...
   return shift->{'errors_seen'} || 0;
 }
 
+# Returns the encoding only if it was recognized as being handled and set
+sub detected_encoding {
+  return shift->{'detected_encoding'};
+}
+
+sub encoding {
+  my $this = shift;
+  return $this->{'encoding'} unless @_;  # GET.
+
+  $this->_handle_encoding_line("=encoding $_[0]");
+  if ($this->{'_processed_encoding'}) {
+    delete $this->{'_processed_encoding'};
+    if(! $this->{'encoding_command_statuses'} ) {
+      DEBUG > 2 and print " CRAZY ERROR: encoding wasn't really handled?!\n";
+    } elsif( $this->{'encoding_command_statuses'}[-1] ) {
+      $this->whine( "=encoding $_[0]",
+         sprintf "Couldn't do %s: %s",
+         $this->{'encoding_command_reqs'  }[-1],
+         $this->{'encoding_command_statuses'}[-1],
+      );
+    } else {
+      DEBUG > 2 and print " (encoding successfully handled.)\n";
+    }
+    return $this->{'encoding'};
+  } else {
+    return undef;
+  }
+}
+
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # Pull in some functions that, for some reason, I expect to see here too:
 BEGIN {

lib/Pod/Simple.pod
@@ -289,6 +289,16 @@ I<Example:>
 
   die "too many errors\n" if $parser->any_errata_seen();
 
+=item C<< $parser->detected_encoding() >>X<detected_encoding>
+
+Return the encoding corresponding to C<< =encoding >>, but only if the
+encoding was recognized and handled.
+
+=item C<< $parser->encoding() >>X<encoding>
+
+Return encoding of the document, even if the encoding is not correctly
+handled.
+
 =item C<< $parser->parse_from_file( $source, $to ) >>X<parse_from_file>
 
 Parses from C<$source> file to C<$to> file. Similar to L<<

lib/Pod/Simple/BlackBox.pm
@@ -91,6 +91,7 @@ sub parse_lines {             # Usage: $parser->parse_lines(@lines)
       if( ($line = $source_line) =~ s/^\xEF\xBB\xBF//s ) {
         DEBUG and print "UTF-8 BOM seen.  Faking a '=encoding utf8'.\n";
         $self->_handle_encoding_line( "=encoding utf8" );
+        delete $self->{'_processed_encoding'};
         $line =~ tr/\n\r//d;
         
       } elsif( $line =~ s/^\xFE\xFF//s ) {
@@ -343,6 +344,7 @@ sub _handle_encoding_line {
     $@ && die( $enc_error =
       "Really unexpected error setting up encoding $e: $@\nAborting"
     );
+    $self->{'detected_encoding'} = $e;
 
   } else {
     my @supported = Pod::Simple::Transcode::->all_encodings;
@@ -373,8 +375,13 @@ sub _handle_encoding_line {
     $self->scream( $self->{'line_count'}, $enc_error );
   }
   push @{ $self->{'encoding_command_statuses'} }, $enc_error;
+  if (defined($self->{'_processed_encoding'})) {
+    # Should never happen
+    die "Nested processed encoding.";
+  }
+  $self->{'_processed_encoding'} = $orig;
 
-  return '=encoding ALREADYDONE';
+  return $line;
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -390,7 +397,11 @@ sub _handle_encoding_second_level {
 
   DEBUG > 2 and print "Ogling encoding directive: =encoding $content\n";
   
-  if($content eq 'ALREADYDONE') {
+  if (defined($self->{'_processed_encoding'})) {
+    #if($content ne $self->{'_processed_encoding'}) {
+    #  Could it happen?
+    #}
+    delete $self->{'_processed_encoding'};
     # It's already been handled.  Check for errors.
     if(! $self->{'encoding_command_statuses'} ) {
       DEBUG > 2 and print " CRAZY ERROR: It wasn't really handled?!\n";
@@ -793,8 +804,7 @@ sub _ponder_paragraph_buffer {
       } elsif($para_type eq '=encoding') {
         # Not actually acted on here, but we catch errors here.
         $self->_handle_encoding_second_level($para);
-
-        next;  # and skip
+        $para_type = 'Plain';
       } elsif($para_type eq '~Verbatim') {
         $para->[0] = 'Verbatim';
         $para_type = '?Verbatim';

t/corpus/2202jp.xml
@@ -8,6 +8,9 @@
   <head1 start_line="7">
     DESCRIPTION
   </head1>
+  <encoding start_line="9">
+    iso-2022-jp
+  </encoding>
   <Para start_line="11">
     This is a test Pod document in ISO-2202-JP. Its content is some Japanese
     haiku by famous poets.

t/corpus/2202jpx.xml
@@ -8,6 +8,9 @@
   <head1 start_line="7">
     DESCRIPTION
   </head1>
+  <encoding start_line="9">
+    iso-2022-jp
+  </encoding>
   <Para start_line="11">
     This is a test Pod document in ISO-2202-JP. Its content is some Japanese
     haiku by famous poets.

t/corpus/2202jpy.xml
@@ -8,6 +8,9 @@
   <head1 start_line="6">
     DESCRIPTION
   </head1>
+  <encoding start_line="8">
+    iso-2022-jp
+  </encoding>
   <Para start_line="10">
     This is a test Pod document in ISO-2202-JP. Its content is some Japanese
     haiku by famous poets.

t/corpus/2202jpz.xml
@@ -8,6 +8,9 @@
   <head1 start_line="6">
     DESCRIPTION
   </head1>
+  <encoding start_line="8">
+    iso-2022-jp
+  </encoding>
   <Para start_line="10">
     This is a test Pod document in ISO-2202-JP.
   </Para>

t/corpus/8859_7.xml
@@ -1,4 +1,7 @@
 <Document start_line="2">
+  <encoding start_line="2">
+    iso-8859-7
+  </encoding>
   <head1 start_line="4">
     NAME
   </head1>

commit 26ade9da55c245f16952049216b269d8d8f4fa5f

Add regression test for encoding behavior.
Broken in fc97ecd.

t/xhtml01.t
@@ -8,8 +8,8 @@ BEGIN {
 
 use strict;
 use lib '../lib';
-#use Test::More tests => 56;
-use Test::More 'no_plan';
+use Test::More tests => 61;
+#use Test::More 'no_plan';
 
 use_ok('Pod::Simple::XHTML') or exit;
 
@@ -695,6 +695,14 @@ EOPOD
 my $T = $use_html_entities ? 84 : 'x54';
 is($results, <<"EOHTML", 'HTML Entities should be only for specified characters');
 <p>&#$T;his is Anna's &quot;Answer&quot; to the &lt;q&gt;Question&lt;/q&gt;.</p>
+
+EOHTML
+
+  # Keep =encoding out of content.
+  initialize($parser, $results);
+  $parser->parse_string_document("=encoding utf-8\n\n=head1 NAME\n");
+  is($results, <<"EOHTML", 'Encoding should not be in content')
+<h1 id="NAME">NAME</h1>
 
 EOHTML
 