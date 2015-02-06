7e9f3093b52a10e0c721415b82ffcb88939e5a43
Karl Williamson <khw@cpan.org>
2015-02-06 11:12:59 -0700

    t/ascii_order.pl: Don't crash on early Perl EBCDICs
    These would otherwise call an undefined function.  Just return instead
    of doing that, which leads to incorrect results, but it's better than
    crashing.

--- t/ascii_order.pl
+++ t/ascii_order.pl
@@ -4,7 +4,8 @@ sub native_to_uni($) {  # Convert from platform character set to Unicode
                         # (which is the same as ASCII)
     my $string = shift;
 
-    return $string if ord("A") == 65;
+    return $string if ord("A") == 65
+                      || $] lt 5.007_003; # Doesn't work on early EBCDIC Perls
     my $output = "";
     for my $i (0 .. length($string) - 1) {
         $output .= chr(utf8::native_to_unicode(ord(substr($string, $i, 1))));
