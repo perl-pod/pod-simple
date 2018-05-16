45177d7be792711d9fd60121440bf1b2a8fbb278
Karl Williamson <khw@cpan.org>
2018-05-15 21:46:52 -0600

    BlackBox.pm: Preserve special case for =item * foo bar
    It is legal to say
    
    =item * This is text on the same line as an asterisk
    
    It is not documented in perlpodspec, but that asterisk is specially
    handled to turn into a bullet.  Russ Allberry in <87y3gtcwa2.fsf@hope.eyrie.org>
    indicates this has been the case for a very long time.  So we are
    preserving the behavior.
    
    But this requires special handling in JustPod, and this commit stops the
    deletion of the hash element that indicates special handling is needed,
    so that JustPod can know about it.

--- lib/Pod/Simple/BlackBox.pm
+++ lib/Pod/Simple/BlackBox.pm
@@ -1050,7 +1050,7 @@ sub _ponder_paragraph_buffer {
 
             if( $para->[1]{'~_freaky_para_hack'} ) {
               DEBUG and print STDERR "Accomodating '=item * Foo' tolerance hack.\n";
-              push @$para, delete $para->[1]{'~_freaky_para_hack'};
+              push @$para, $para->[1]{'~_freaky_para_hack'};
             }
 
           } elsif($item_type eq 'number') {
@@ -1661,7 +1661,7 @@ sub _ponder_item {
 
       if( $para->[1]{'~_freaky_para_hack'} ) {
         DEBUG and print STDERR "Accomodating '=item * Foo' tolerance hack.\n";
-        push @$para, delete $para->[1]{'~_freaky_para_hack'};
+        push @$para, $para->[1]{'~_freaky_para_hack'};
       }
 
     } elsif($item_type eq 'number') {
