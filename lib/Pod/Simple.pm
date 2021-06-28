ae259a7e827303938baed45e6ac4369dd6550c4d
Karl Williamson <khw@cpan.org>
2021-06-28 15:49:24 -0600

    Update files to version 3.43

--- lib/Pod/Simple.pm
+++ lib/Pod/Simple.pm
@@ -19,7 +19,7 @@ use vars qw(
 );
 
 @ISA = ('Pod::Simple::BlackBox');
-$VERSION = '3.42';
+$VERSION = '3.43';
 
 @Known_formatting_codes = qw(I B C L E F S X Z); 
 %Known_formatting_codes = map(($_=>1), @Known_formatting_codes);
