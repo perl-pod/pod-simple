d653017ac89cb5c80ac6429d222b99d0a079de84
James E Keenan <jkeenan@cpan.org>
2024-07-17 16:19:03 -0400

    search.cpan.org is dead
    So we probably don't need it in ACKNOWLEDGEMENTS any more.

--- lib/Pod/Simple/XHTML.pm
+++ lib/Pod/Simple/XHTML.pm
@@ -910,9 +910,6 @@ merchantability or fitness for a particular purpose.
 Thanks to L<Hurricane Electric|http://he.net/> for permission to use its
 L<Linux man pages online|http://man.he.net/> site for man page links.
 
-Thanks to L<search.cpan.org|http://search.cpan.org/> for permission to use the
-site for Perl module links.
-
 =head1 AUTHOR
 
 Pod::Simpele::XHTML was created by Allison Randal <allison@perl.org>.
